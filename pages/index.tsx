b1bc131a55647a39d812f682fa7949411460c1bf
Sara Evans <saraevs@gmail.com>
2023-09-02 14:17:49 +0100

    style: add background container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  background-image: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  padding: 16px;
+`
+
 export default function Home() {
   return (
-    <>
-      <main>
-      </main>
-    </>
+    <Container>
+      {'Placeholder'}
+    </Container>
   )
 }
