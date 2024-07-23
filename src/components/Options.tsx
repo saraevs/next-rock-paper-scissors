62be7a602eaf23bb54b62190e50398d9fe17b92d
Sara Evans <saraevs@gmail.com>
2024-07-23 09:46:05 +0100

    style: add background triangle on desktop

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { CircleButton } from "./CircleButton";
 import styled from "styled-components";
 import { MOBILE_SCREEN_WIDTH_PX, options } from "../constants";
 import { Option } from "../types";
+import backgroundTriangle from "public/images/bg-triangle.svg";
 
 const StyledOptions = styled.div`
   display: flex;
   width: 50%;
   justify-content: center;
+  align-items: center;
   flex-wrap: wrap;
   gap: 100px;
+  background-image: url(${backgroundTriangle.src});
+  background-repeat: no-repeat;
+  background-position: center;
 
   @media (max-width: ${MOBILE_SCREEN_WIDTH_PX}) {
     width: 100%;
