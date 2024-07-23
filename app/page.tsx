47de06a6112b0860d1774d1e5d58c5a8190cc3ab
Sara Evans <saraevs@gmail.com>
2024-07-23 21:45:09 +0100

    chore: add favicon and page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import HomePage from "./home-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rock, paper, scissors",
+};
 
 export default async function Page() {
   return <HomePage />;
